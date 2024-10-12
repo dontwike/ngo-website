840d08201a3a0285bb139629f46c0f9996f8f13d
Pranjal Goyal <100211916+dontwike@users.noreply.github.com>
2024-10-12 17:41:53 +0530

    Herosection fix text

--- src/page/HeroSection.jsx
+++ src/page/HeroSection.jsx
@@ -7,11 +7,11 @@ const HeroSection = () => {
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="relative z-10 flex items-center justify-start h-full px-4 text-white md:ml-20">
                 <div className="max-w-3xl">
-                    <h1 className="text-4xl font-bold md:text-6xl">Transforming Surplus <br /> into Support</h1>
+                    <h1 className="text-3xl font-bold md:text-6xl">Transforming Surplus <br /> into Support</h1>
                     <p className="mt-4 text-lg md:text-xl">
                         To fight food insecurity by leveraging the surplus <br />food from the hospitality industry.
                     </p>
-                    <div className="mt-6 space-x-4">
+                    <div className="mt-10 space-x-4">
                         <button className="px-3 py-2 md:px-8 md:py-2 bg-gray-300 text-gray-800 font-semibold rounded-lg hover:bg-gray-400">
                             Eternal Merch
                         </button>
